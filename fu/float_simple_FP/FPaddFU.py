524085db51e88aea811b8cd727143215c525b959
Ron Jokai <ron.jokai@asu.edu>
2024-01-08 23:03:37 -0700

    [FP and fused units] Renamed units.

--- fu/float_simple_FP/FPaddFU.py
+++ fu/float_simple_FP/FPaddFU.py
@@ -1,57 +1,54 @@
 """
 ==========================================================================
-ALUgenMAC2nd.py
+FPaddFU.py
 ==========================================================================
 Floating point add unit.
 
 Author : Yanghui Ou & Ron Jokai
-  Date : Jan 6, 2024
+  Date : Jan 4, 2024
 """
 
 from pymtl3                                import *
 from ...lib.ifcs                           import SendIfcRTL, RecvIfcRTL
 from ...lib.opt_type                       import *
 from ..basic.Fu                            import Fu
-from ..pymtl3_Ron_fixed.ALUgenMACRTL       import ALUgenMAC
+from ..pymtl3_Ron_FP.FPaddRTL              import FPadd
 
-class ALUgenMAC2nd( Fu ):
+class FPaddFU( Fu ):
   def construct( s, DataType, PredicateType, CtrlType,
-                 num_inports, num_outports, data_mem_size):
-    super( ALUgenMAC2nd, s ).construct( DataType, PredicateType, CtrlType,
+                 num_inports, num_outports, data_mem_size, exp_nbits = 4,
+                 sig_nbits = 11 ):
+    super( FPadd2nd, s ).construct( DataType, PredicateType, CtrlType,
                                    num_inports, num_outports,
                                    data_mem_size )
 
     # Local parameters
-    assert DataType.get_field_type( 'payload' ).nbits == 16
+    assert DataType.get_field_type( 'payload' ).nbits == exp_nbits + sig_nbits + 1
 
-    num_entries = 3
+    num_entries = 2
     FuInType    = mk_bits( clog2( num_inports + 1 ) )
     CountType   = mk_bits( clog2( num_entries + 1 ) )
+    # TODO: parameterize rounding mode
+    s.rounding_mode = 0b000
+    s.FLOATING_ONE = concat(
+        b1(0), mk_bits(exp_nbits)(2**(exp_nbits-1)-1),
+        mk_bits(sig_nbits)() )
 
     # Components
-    s.fALU = ALUgenMAC()
-    s.fALU.rhs_2 //= lambda:   0 if s.recv_opt.msg.ctrl == OPT_ADD     else \
-                             ( 1 if s.recv_opt.msg.ctrl == OPT_SUB     else \
-                             ( 3 if s.recv_opt.msg.ctrl == OPT_LT      else \
-                             ( 7 if s.recv_opt.msg.ctrl == OPT_GTE     else \
-                             (10 if s.recv_opt.msg.ctrl == OPT_GT      else \
-                             (14 if s.recv_opt.msg.ctrl == OPT_LTE     else \
-                             (16 if s.recv_opt.msg.ctrl == OPT_MUL     else \
-                             (48 if s.recv_opt.msg.ctrl == OPT_MUL_ADD else 0)))))))
+    s.fadd = FPadd()
+    #s.fadd.roundingMode //= s.rounding_mode
+    #s.fadd.subOp //= lambda: s.recv_opt.msg.ctrl == OPT_FSUB
 
     # Wires
     s.in0 = Wire( FuInType )
     s.in1 = Wire( FuInType )
-    s.in2 = Wire( FuInType )
 
     idx_nbits = clog2( num_inports )
     s.in0_idx = Wire( idx_nbits )
     s.in1_idx = Wire( idx_nbits )
-    s.in2_idx = Wire( idx_nbits )
 
     s.in0_idx //= s.in0[0:idx_nbits]
     s.in1_idx //= s.in1[0:idx_nbits]
-    s.in2_idx //= s.in2[0:idx_nbits]
 
     @update
     def comb_logic():
@@ -59,7 +56,6 @@ class ALUgenMAC2nd( Fu ):
       # For pick input register
       s.in0 @= 0
       s.in1 @= 0
-      s.in2 @= 0
       for i in range( num_inports ):
         s.recv_in[i].rdy @= b1( 0 )
 
@@ -76,30 +72,21 @@ class ALUgenMAC2nd( Fu ):
         if s.recv_opt.msg.fu_in[1] != 0:
           s.in1 @= zext(s.recv_opt.msg.fu_in[1] - 1, FuInType)
           s.recv_in[s.in1_idx].rdy @= b1( 1 )
-        if s.recv_opt.msg.fu_in[2] != 0:
-          s.in2 @= zext(s.recv_opt.msg.fu_in[2] - 1, FuInType)
-          s.recv_in[s.in2_idx].rdy @= b1( 1 )
         if s.recv_opt.msg.predicate == b1( 1 ):
           s.recv_predicate.rdy @= b1( 1 )
 
       s.send_out[0].msg.predicate @= s.recv_in[s.in0_idx].msg.predicate & \
-                                     s.recv_in[s.in1_idx].msg.predicate & \
-                                     s.recv_in[s.in2_idx].msg.predicate
+                                     s.recv_in[s.in1_idx].msg.predicate
 
-      # TODO RJ Here we need to block predicate propagation for the third port if ctrl != MAC.
       #if s.recv_opt.msg.ctrl == OPT_FADD:
-      s.fALU.rhs_0  @= s.recv_in[s.in0_idx].msg.payload
-      s.fALU.rhs_1  @= s.recv_in[s.in1_idx].msg.payload
-      s.fALU.rhs_1b @= s.recv_in[s.in2_idx].msg.payload
+      s.fadd.rhs_0 @= s.recv_in[s.in0_idx].msg.payload
+      s.fadd.rhs_1 @= s.recv_in[s.in1_idx].msg.payload
       s.send_out[0].msg.predicate @= s.recv_in[s.in0_idx].msg.predicate & \
-                                     s.recv_in[s.in1_idx].msg.predicate & \
-                                     s.recv_in[s.in2_idx].msg.predicate
+                                     s.recv_in[s.in1_idx].msg.predicate
       if s.recv_opt.en & ( (s.recv_in_count[s.in0_idx] == 0) | \
-                           (s.recv_in_count[s.in1_idx] == 0) | \
-                           (s.recv_in_count[s.in2_idx] == 0) ):
+                           (s.recv_in_count[s.in1_idx] == 0) ):
         s.recv_in[s.in0_idx].rdy @= b1( 0 )
         s.recv_in[s.in1_idx].rdy @= b1( 0 )
-        s.recv_in[s.in2_idx].rdy @= b1( 0 )
         s.send_out[0].msg.predicate @= b1( 0 )
 
       #elif s.recv_opt.msg.ctrl == OPT_FADD_CONST:
@@ -129,5 +116,4 @@ class ALUgenMAC2nd( Fu ):
         s.send_out[0].msg.predicate @= s.send_out[0].msg.predicate & \
                                        s.recv_predicate.msg.predicate
 
-      s.send_out[0].msg.payload @= s.fALU.lhs_0
-
+      s.send_out[0].msg.payload @= s.fadd.lhs_0
--- fu/float_simple_FP/test/FPaddFU_test.py
+++ fu/float_simple_FP/test/FPaddFU_test.py
@@ -1,10 +1,11 @@
 """
 ==========================================================================
-_test.py
+FPaddFU_test.py
 ==========================================================================
+Test cases for floating point add unit.
 
-Author : RJ
-  Date : Jan 6, 2024
+Author : Yanghui Ou
+  Date : Aug 8, 2023
 """
 
 from pymtl3                       import *
@@ -15,28 +16,31 @@ from ....lib.test_srcs            import TestSrcRTL
 from ....lib.opt_type             import *
 from ....lib.messages             import *
 from ....mem.const.ConstQueueRTL  import ConstQueueRTL
-from ..ALUgenMAC2nd               import ALUgenMAC2nd
+from ..FPaddFU                    import FPaddFU
+from ...pymtl3_hardfloat.HardFloat.converter_funcs import (floatToFN,
+                                                           fNToFloat)
+
+round_near_even   = 0b000
 
 def test_elaborate( cmdline_opts ):
   DataType      = mk_data( 16, 1 )
   PredicateType = mk_predicate( 1, 1 )
-  ConfigType    = mk_ctrl(3, 3, 1)
+  ConfigType    = mk_ctrl()
   data_mem_size = 8
-  num_inports   = 3
+  num_inports   = 2
   num_outports  = 1
   FuInType      = mk_bits( clog2( num_inports + 1 ) )
   pickRegister  = [ FuInType( x+1 ) for x in range( num_inports ) ]
   src_in0       = [ DataType(1, 1), DataType(7, 1), DataType(4, 1) ]
   src_in1       = [ DataType(2, 1), DataType(3, 1), DataType(1, 1) ]
-  src_in2       = [ DataType(2, 1), DataType(3, 1), DataType(1, 1) ]
   src_predicate = [ PredicateType(1, 0), PredicateType(1, 0), PredicateType(1, 1) ]
   src_const     = [ DataType(5, 1), DataType(0, 0), DataType(7, 1) ]
   sink_out      = [ DataType(6, 0), DataType(4, 0), DataType(11, 1) ]
   src_opt       = [ ConfigType( OPT_ADD_CONST, b1( 1 ), pickRegister ),
                     ConfigType( OPT_SUB,       b1( 1 ), pickRegister ),
                     ConfigType( OPT_ADD_CONST, b1( 1 ), pickRegister ) ]
-  dut = ALUgenMAC2nd( DataType, PredicateType, ConfigType, num_inports,
-                      num_outports, data_mem_size )
+  dut = FPadd2nd( DataType, PredicateType, ConfigType, num_inports,
+                  num_outports, data_mem_size, exp_nbits = 4, sig_nbits = 11 )
   dut = config_model_with_cmdline_opts( dut, cmdline_opts, duts=[] )
 
 #-------------------------------------------------------------------------
@@ -47,12 +51,11 @@ class TestHarness( Component ):
 
   def construct( s, FunctionUnit, DataType, PredicateType, ConfigType,
                  num_inports, num_outports, data_mem_size,
-                 src0_msgs, src1_msgs, src2_msgs, src_predicate, src_const,
+                 src0_msgs, src1_msgs, src_predicate, src_const,
                  ctrl_msgs, sink_msgs ):
 
     s.src_in0       = TestSrcRTL( DataType,      src0_msgs     )
     s.src_in1       = TestSrcRTL( DataType,      src1_msgs     )
-    s.src_in2       = TestSrcRTL( DataType,      src2_msgs     )
     s.src_predicate = TestSrcRTL( PredicateType, src_predicate )
     s.src_opt       = TestSrcRTL( ConfigType,    ctrl_msgs     )
     s.sink_out      = TestSinkRTL( DataType,     sink_msgs     )
@@ -66,7 +69,6 @@ class TestHarness( Component ):
 
     connect( s.src_in0.send,       s.dut.recv_in[0]         )
     connect( s.src_in1.send,       s.dut.recv_in[1]         )
-    connect( s.src_in2.send,       s.dut.recv_in[2]         )
     connect( s.src_predicate.send, s.dut.recv_predicate     )
     connect( s.dut.recv_const,     s.const_queue.send_const )
     connect( s.src_opt.send,       s.dut.recv_opt           )
@@ -79,34 +81,36 @@ class TestHarness( Component ):
   def line_trace( s ):
     return s.dut.line_trace()
 
+def mk_float_to_bits_fn( DataType, exp_nbits = 4, sig_nbits = 11 ):
+  return lambda f_value, predicate: (
+      DataType( floatToFN( f_value,
+                           precision = 1 + exp_nbits + sig_nbits ),
+                predicate ) )
+
 def test_add_basic(cmdline_opts):
-  FU            = ALUgenMAC2nd
+  FU            = FPaddFU
   exp_nbits     = 4
   sig_nbits     = 11
-  DataType      = mk_data( 16, 1 )
+  DataType      = mk_data( 1 + exp_nbits + sig_nbits, 1 )
+  f2b           = mk_float_to_bits_fn( DataType, exp_nbits, sig_nbits )
   PredicateType = mk_predicate( 1, 1 )
-  ConfigType    = mk_ctrl(3, 3, 1)
+  ConfigType    = mk_ctrl()
   data_mem_size = 8
-  num_inports   = 3
+  num_inports   = 2
   num_outports  = 1
   FuInType      = mk_bits( clog2( num_inports + 1 ) )
   pickRegister  = [ FuInType( x+1 ) for x in range( num_inports ) ]
-  src_in0       = [ DataType(1, 1), DataType(2, 1), DataType(3, 1),DataType(3, 1),DataType(3, 1),DataType(3, 1), DataType(3, 1) ]
-  src_in1       = [ DataType(1, 1), DataType(2, 1), DataType(3, 1),DataType(3, 1),DataType(3, 1),DataType(3, 1), DataType(3, 1) ]
-  src_in2       = [ DataType(1, 1), DataType(2, 1), DataType(3, 1),DataType(3, 1),DataType(3, 1),DataType(3, 1), DataType(3, 1) ]
-  src_predicate = [ PredicateType(1, 1), PredicateType(1, 1), PredicateType(1, 1) ]
-  src_const     = [ DataType(1, 1), DataType(2, 1), DataType(3, 1) ]
-  sink_out      = [ DataType(2, 1), DataType(0, 1), DataType(0, 1),DataType(1, 1),DataType(0, 1),DataType(1, 1), DataType(12, 1) ]
-  src_opt       = [ ConfigType( OPT_ADD, b1( 1 ), pickRegister ),
-                    ConfigType( OPT_SUB, b1( 1 ), pickRegister ),
-                    ConfigType( OPT_LT, b1( 1 ), pickRegister ),
-                    ConfigType( OPT_LTE, b1( 1 ), pickRegister ),
-                    ConfigType( OPT_GT, b1( 1 ), pickRegister ),
-                    ConfigType( OPT_GTE, b1( 1 ), pickRegister ),
-                    ConfigType( OPT_MUL_ADD, b1( 1 ), pickRegister ) ]
+  src_in0       = [ f2b(1.1, 1), f2b(7.7, 1), f2b(4.4, 1) ]
+  src_in1       = [ f2b(2.2, 1), f2b(3.3, 1), f2b(1.1, 1) ]
+  src_predicate = [ PredicateType(1, 0), PredicateType(1, 0), PredicateType(1, 1) ]
+  src_const     = [ f2b(5.5, 1), f2b(0, 0), f2b(7.7, 1) ]
+  sink_out      = [ f2b(6.602, 0), f2b(4.4, 0), f2b(12.1, 1) ] # 6.6 -> 6.602
+  src_opt       = [ ConfigType( OPT_FADD_CONST, b1( 1 ), pickRegister ),
+                    ConfigType( OPT_FSUB,       b1( 1 ), pickRegister ),
+                    ConfigType( OPT_FADD_CONST, b1( 1 ), pickRegister ) ]
   th = TestHarness( FU, DataType, PredicateType, ConfigType,
                     num_inports, num_outports, data_mem_size,
-                    src_in0, src_in1, src_in2, src_predicate, src_const, src_opt,
+                    src_in0, src_in1, src_predicate, src_const, src_opt,
                     sink_out )
   run_sim( th )
 
