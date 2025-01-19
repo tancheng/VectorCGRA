895970db414f7fcae6fbdb9fa967422a77a80030
yyan7223 <22B305016@stu.hit.edu.cn>
2025-01-19 04:29:32 -1000

    Initial commit for ctrl&data preloading.

--- controller/ControllerRTL.py
+++ controller/ControllerRTL.py
@@ -68,7 +68,7 @@ class ControllerRTL(Component):
     # allow one request be sent out per cycle).
     # TODO: Include other cmd requests, e.g., dynamic rescheduling,
     # termination).
-    s.crossbar = XbarBypassQueueRTL(NocPktType, 3, 1)
+    s.crossbar = XbarBypassQueueRTL(NocPktType, 4, 1)
 
     s.recv_ctrl_pkt_queue = NormalQueueRTL(CtrlPktType)
 
@@ -131,14 +131,16 @@ class ControllerRTL(Component):
     # other CGRAs can be delivered via the NoC across CGRAs. Note that the packet
     # format can be in a universal fashion to support both data and config. Later
     # on, the format can be packet-based or flit-based.
-    s.recv_from_cpu_ctrl_pkt //= s.recv_ctrl_pkt_queue.recv
-    s.recv_ctrl_pkt_queue.send //= s.send_to_ctrl_ring_ctrl_pkt
+    # s.recv_from_cpu_ctrl_pkt //= s.recv_ctrl_pkt_queue.recv
+
 
     @update
     def update_received_msg():
       kLoadRequestInportIdx = 0
       kLoadResponseInportIdx = 1
       kStoreRequestInportIdx = 2
+      kPreloadCtrlIdx = 3
+      kPreloaDataIdx = 4
 
       # For the load request from local tiles.
       s.crossbar.recv[kLoadRequestInportIdx].val @= s.recv_from_tile_load_request_pkt_queue.send.val
@@ -200,6 +202,30 @@ class ControllerRTL(Component):
                      s.recv_from_tile_load_response_pkt_queue.send.msg.predicate,
                      0)
 
+
+      # For the ctrl preloading.
+      s.crossbar.recv[kPreloadCtrlIdx].val @= \
+          s.recv_ctrl_pkt_queue.send.val
+      s.recv_ctrl_pkt_queue.send.rdy @= s.crossbar.recv[kPreloadCtrlIdx].rdy
+      s.crossbar.recv[kPreloadCtrlIdx].msg @= \
+          NocPktType(controller_id,
+                     0,
+                     s.idTo2d_x_lut[controller_id], # src_x
+                     s.idTo2d_y_lut[controller_id], # src_y
+                     0, # dst_x
+                     0, # dst_y
+                     0,
+                     0,
+                     CMD_PRELOAD_CTRL,
+                     # TODO: CtrlPktType is incompatible with NocPktType, e.g., how to send other ctrl signals like fu_in?
+                     0,
+                     0,
+                     0,
+                     0)
+
+      # For the data preloading.
+      # TODO: Where deos the preloading data for other controllers come from, recv_from_noc or recv_from_tile_store_request_pkt?
+     
       # TODO: For the other cmd types.
 
 
@@ -244,11 +270,66 @@ class ControllerRTL(Component):
             s.send_to_tile_load_response_data_queue.recv.msg @= \
                 CGRADataType(received_pkt.data, received_pkt.predicate, 0, 0)
             s.send_to_tile_load_response_data_queue.recv.val @= 1
-
+     
         # else:
         #   # TODO: Handle other cmd types.
         #   assert(False)
 
+
+      if controller_id == 0:
+        # Preloads ctrl to the 1st controller.
+        if s.recv_ctrl_pkt_queue.recv.rdy:
+          s.recv_ctrl_pkt_queue.recv.val @= s.recv_from_cpu_ctrl_pkt.val
+          s.recv_ctrl_pkt_queue.recv.msg @= s.recv_from_cpu_ctrl_pkt.msg
+        # Preloads data to the 1st controller.
+        if s.send_to_tile_store_request_addr_queue.recv.rdy & \
+          s.send_to_tile_store_request_data_queue.recv.rdy:
+          s.recv_from_cpu_ctrl_pkt.rdy @= 1
+          # TODO: Renames recv_from_cpu_ctrl_pkt to recv_from_cpu_pkt, add cmd, data, and addr field for preloading.
+          #s.send_to_tile_store_request_addr_queue.recv.msg @= \
+          #      CGRAAddrType(s.recv_from_cpu_ctrl_pkt.msg.addr)
+          #s.send_to_tile_store_request_data_queue.recv.msg @= \
+          #      CGRADataType(s.recv_from_cpu_ctrl_pkt.msg.data, s.recv_from_cpu_ctrl_pkt.msg.predicate, 0, 0)
+          s.send_to_tile_store_request_addr_queue.recv.msg @= CGRAAddrType()
+          s.send_to_tile_store_request_data_queue.recv.msg @= CGRADataType()
+          s.send_to_tile_store_request_addr_queue.recv.val @= s.recv_from_cpu_ctrl_pkt.val
+          s.send_to_tile_store_request_data_queue.recv.val @= s.recv_from_cpu_ctrl_pkt.val
+
+      else:
+        # Preloads ctrl to the rest of controllers through NoC.
+        if s.recv_from_noc.msg.cmd == CMD_PRELOAD_CTRL:
+          if s.recv_ctrl_pkt_queue.recv.rdy:
+            s.recv_ctrl_pkt_queue.recv.val @= s.recv_from_noc.val
+            # TODO: CtrlPktType of recv_ctrl_pkt_queue is incompatible with NocPktType of recv_from_noc.
+            #s.recv_ctrl_pkt_queue.recv.msg @= CtrlPktType(received_pkt.src,
+            #                                                  received_pkt.dst,
+            #                                                  received_pkt.opaque,
+            #                                                  received_pkt.cmd,
+            #                                                  received_pkt.addr,
+            #                                                  received_pkt.data,
+            #                                                  received_pkt.predicate)
+        # Preloads data to the rest of controllers through NoC.
+        elif s.recv_from_noc.msg.cmd == CMD_PRELOAD_DATA:
+          if s.send_to_tile_store_request_addr_queue.recv.rdy & \
+             s.send_to_tile_store_request_data_queue.recv.rdy:
+            s.recv_from_noc.rdy @= 1
+            s.send_to_tile_store_request_addr_queue.recv.msg @= \
+                CGRAAddrType(received_pkt.addr)
+            s.send_to_tile_store_request_data_queue.recv.msg @= \
+                CGRADataType(received_pkt.data, received_pkt.predicate, 0, 0)
+            s.send_to_tile_store_request_addr_queue.recv.val @= s.recv_from_noc.val
+            s.send_to_tile_store_request_data_queue.recv.val @= s.recv_from_noc.val
+
+    @update
+    def update_send_to_ctrl_ring_msg():
+      # Delivers the ctrl pkt that belongs to current controller to the CGRA tiles.
+      if s.recv_ctrl_pkt_queue.send.msg.dst == controller_id:
+        if s.send_to_ctrl_ring_ctrl_pkt.rdy:
+          s.send_to_ctrl_ring_ctrl_pkt.val @= 1
+          s.send_to_ctrl_ring_ctrl_pkt.msg @= s.recv_ctrl_pkt_queue.send.msg
+      else:
+          s.send_to_ctrl_ring_ctrl_pkt.val @= 0
+          s.send_to_ctrl_ring_ctrl_pkt.msg @= CtrlPktType()
 
     @update
     def update_sending_to_noc_msg():
