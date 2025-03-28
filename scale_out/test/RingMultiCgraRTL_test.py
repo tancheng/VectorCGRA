f3d5aecf5242996ce263b20f4e70c430e83ed5c3
yuqisun <yuqi.sun@outlook.com>
2025-03-28 17:07:37 +0800

    Add num_tiles for target id in ctrl ring.

--- scale_out/test/RingMultiCgraRTL_test.py
+++ scale_out/test/RingMultiCgraRTL_test.py
@@ -53,7 +53,7 @@ class TestHarness(Component):
     s.complete_signal_sink_out.recv //= s.dut.send_to_cpu_pkt
 
   def done(s):
-    return s.src_ctrl_pkt.done()
+    return s.src_ctrl_pkt.done() and s.complete_signal_sink_out.done()
 
   def line_trace(s):
     return s.dut.line_trace()
@@ -104,7 +104,7 @@ def test_homo_2x2(cmdline_opts):
   predicate_nbits = 1
 
   CtrlPktType = \
-        mk_intra_cgra_pkt(width * height,
+        mk_intra_cgra_pkt(num_tiles,
                         cgra_id_nbits,
                         num_commands,
                         ctrl_mem_size,
@@ -128,7 +128,7 @@ def test_homo_2x2(cmdline_opts):
 
   NocPktType = mk_multi_cgra_noc_pkt(ncols = num_terminals,
                                      nrows = 1,
-                                     ntiles = width * height,
+                                     ntiles = num_tiles,
                                      addr_nbits = data_addr_nbits,
                                      data_nbits = 32,
                                      predicate_nbits = 1,
@@ -201,7 +201,7 @@ def test_homo_2x2(cmdline_opts):
                        [FuOutType(0), FuOutType(0), FuOutType(0), FuOutType(0),
                         FuOutType(1), FuOutType(1), FuOutType(1), FuOutType(1)])
       ] for i in range(num_tiles)]
-  complete_signal_sink_out = [CtrlPktType(0, 0, 0, 0, 0, ctrl_action = CMD_COMPLETE)]
+  complete_signal_sink_out = [CtrlPktType(0, 0, num_tiles, 0, 1, ctrl_action = CMD_COMPLETE)]
 
   src_ctrl_pkt = []
   for opt_per_tile in src_opt_per_tile:
