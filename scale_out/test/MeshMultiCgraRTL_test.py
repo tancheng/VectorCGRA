d0c0602ff84f1543239bdf0eb06d82c5bef9ee7c
tancheng <tancheng1990@gmail.com>
2025-04-01 02:55:44 +0000

    [WIP] Provide test for cgra_0 load and store on multi-cgra

--- scale_out/test/MeshMultiCgraRTL_test.py
+++ scale_out/test/MeshMultiCgraRTL_test.py
@@ -116,7 +116,7 @@ def test_homo_2x2_2x2(cmdline_opts):
                               (i + 1) * per_cgra_data_size - 1]
 
   cmd_nbits = clog2(NUM_CMDS)
-  num_registers_per_reg_bank = 16
+  RegIdxType = mk_bits(clog2(num_registers_per_reg_bank))
   CmdType = mk_bits(cmd_nbits)
 
   cgra_id_nbits = clog2(num_terminals)
@@ -222,16 +222,81 @@ def test_homo_2x2_2x2(cmdline_opts):
   #                                       cgra_id, src,       dst, opaque, vc, ctrl_action
   complete_signal_sink_out = [CtrlPktType(      0,   0, num_tiles,      0,  1, ctrl_action = CMD_COMPLETE)]
 
-  src_ctrl_pkt = []
-  for opt_per_tile in src_opt_per_tile:
-    src_ctrl_pkt.extend(opt_per_tile)
+  # src_ctrl_pkt = []
+  # for opt_per_tile in src_opt_per_tile:
+  #   src_ctrl_pkt.extend(opt_per_tile)
+  src_ctrl_pkt = \
+      [
+       # Tile 0.
+
+       # Indicates the load address of 2.
+       CtrlPktType(0,      0,  0,  0,    0, ctrl_action = CMD_CONST, data = 2),
+
+                 # cgra_id src dst vc_id opq cmd_type    addr operation predicate
+       CtrlPktType(0,      0,  0,  0,    0,  CMD_CONFIG, 0,   OPT_LD_CONST,  b1(0),
+                   [FuInType(1), FuInType(0), FuInType(0), FuInType(0)],
+                   [TileInType(0), TileInType(0), TileInType(0), TileInType(0),
+                    TileInType(0), TileInType(0), TileInType(0), TileInType(0)],
+
+                   [FuOutType(0), FuOutType(0), FuOutType(0), FuOutType(0),
+                    FuOutType(0), FuOutType(0), FuOutType(0), FuOutType(0)],
+                   ctrl_write_reg_from = [b2(1), b2(0), b2(0), b2(0)],
+                   ctrl_write_reg_idx = [RegIdxType(7), RegIdxType(0), RegIdxType(0), RegIdxType(0)]
+                  ),
+
+       CtrlPktType(0,      0,  0,  0,    0,  CMD_CONFIG, 0,   OPT_INC,  b1(0),
+                   pickRegister,
+                   [TileInType(0), TileInType(0), TileInType(0), TileInType(0),
+                    TileInType(0), TileInType(0), TileInType(0), TileInType(0)],
+
+                   [FuOutType(1), FuOutType(0), FuOutType(0), FuOutType(1),
+                    FuOutType(0), FuOutType(0), FuOutType(0), FuOutType(0)],
+                   ctrl_read_reg_from = [b1(1), b1(0), b1(0), b1(0)],
+                   ctrl_read_reg_idx = [RegIdxType(7), RegIdxType(0), RegIdxType(0), RegIdxType(0)]
+                  ),
+
+       # Tile 2. Note that tile 0 and tile 2 can access the memory, as they are on
+       # the first column.
+
+       # Indicates the store address of 3.
+       CtrlPktType(0,      0,  2,  0,    0, ctrl_action = CMD_CONST, data = 3),
+
+                 # cgra_id src dst vc_id opq cmd_type    addr operation predicate
+       CtrlPktType(0,      0,  2,  0,    0,  CMD_CONFIG, 0,   OPT_STR_CONST,  b1(0),
+                   pickRegister,
+                   [TileInType(0), TileInType(0), TileInType(0), TileInType(0),
+                    # TODO: make below as TileInType(5) to double check.
+                    TileInType(2), TileInType(0), TileInType(0), TileInType(0)],
+
+                   [FuOutType(0), FuOutType(0), FuOutType(0), FuOutType(0),
+                    FuOutType(0), FuOutType(0), FuOutType(0), FuOutType(0)], 0, 0, 0, 0, 0),
+
+       # For launching the two tiles.
+       CtrlPktType(0,      0,  0,  0,    0,  CMD_LAUNCH, 0,   OPT_NAH, b1(0),
+                   pickRegister,
+                   [TileInType(0), TileInType(0), TileInType(0), TileInType(0),
+                    TileInType(0), TileInType(0), TileInType(0), TileInType(0)],
+
+                   [FuOutType(0), FuOutType(0), FuOutType(0), FuOutType(0),
+                    FuOutType(0), FuOutType(0), FuOutType(0), FuOutType(0)], 0, 0, 0, 0, 0),
+       CtrlPktType(0,      0,  2,  0,    0,  CMD_LAUNCH, 0,   OPT_NAH, b1(0),
+                   pickRegister,
+                   [TileInType(0), TileInType(0), TileInType(0), TileInType(0),
+                    TileInType(0), TileInType(0), TileInType(0), TileInType(0)],
+
+                   [FuOutType(0), FuOutType(0), FuOutType(0), FuOutType(0),
+                    FuOutType(0), FuOutType(0), FuOutType(0), FuOutType(0)], 0, 0, 0, 0, 0)
+      ]
+
+  # We only needs 2 steps to finish this test.
+  ctrl_steps = 2
 
   th = TestHarness(DUT, FunctionUnit, FuList, DataType, PredicateType, CtrlPktType,
                    CtrlSignalType, NocPktType, CmdType, cgra_rows, cgra_columns,
                    x_tiles, y_tiles, ctrl_mem_size, data_mem_size_global,
                    data_mem_size_per_bank, num_banks_per_cgra,
                    num_registers_per_reg_bank, src_ctrl_pkt,
-                   ctrl_mem_size, controller2addr_map, complete_signal_sink_out)
+                   ctrl_steps, controller2addr_map, complete_signal_sink_out)
   th.elaborate()
   th.dut.set_metadata(VerilogVerilatorImportPass.vl_Wno_list,
                       ['UNSIGNED', 'UNOPTFLAT', 'WIDTH', 'WIDTHCONCAT',
