17664fdc79f249876bd2141b80012e02de4e17d0
Cheng Tan <tancheng1990@gmail.com>
2025-04-15 18:47:10 -0700

    Update SeqMulShifterRTL.py

--- fu/double/SeqMulShifterRTL.py
+++ fu/double/SeqMulShifterRTL.py
@@ -28,13 +28,13 @@ class SeqMulShifterRTL(TwoSeqCombo):
     @update
     def update_opt():
 
+      s.Fu0.recv_opt.msg @= s.recv_opt.msg
+      s.Fu1.recv_opt.msg @= s.recv_opt.msg
+  
       s.Fu0.recv_opt.msg.fu_in[0] @= 1
       s.Fu0.recv_opt.msg.fu_in[1] @= 2
       s.Fu1.recv_opt.msg.fu_in[0] @= 1
       s.Fu1.recv_opt.msg.fu_in[1] @= 2
-
-      s.Fu0.recv_opt.msg @= s.recv_opt.msg
-      s.Fu1.recv_opt.msg @= s.recv_opt.msg
   
       if s.recv_opt.msg.operation == OPT_MUL_LLS:
         s.Fu0.recv_opt.msg.operation @= OPT_MUL
