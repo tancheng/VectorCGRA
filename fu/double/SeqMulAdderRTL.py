62bfa89370f00c4494415c2b523f1b8f906384f2
Cheng Tan <tancheng1990@gmail.com>
2025-04-15 18:46:57 -0700

    Update SeqMulAdderRTL.py

--- fu/double/SeqMulAdderRTL.py
+++ fu/double/SeqMulAdderRTL.py
@@ -28,13 +28,13 @@ class SeqMulAdderRTL(TwoSeqCombo):
     @update
     def update_opt():
 
+      s.Fu0.recv_opt.msg @= s.recv_opt.msg
+      s.Fu1.recv_opt.msg @= s.recv_opt.msg
+  
       s.Fu0.recv_opt.msg.fu_in[0] @= 1
       s.Fu0.recv_opt.msg.fu_in[1] @= 2
       s.Fu1.recv_opt.msg.fu_in[0] @= 1
       s.Fu1.recv_opt.msg.fu_in[1] @= 2
-
-      s.Fu0.recv_opt.msg @= s.recv_opt.msg
-      s.Fu1.recv_opt.msg @= s.recv_opt.msg
   
       if s.recv_opt.msg.operation == OPT_MUL_ADD:
         s.Fu0.recv_opt.msg.operation @= OPT_MUL
