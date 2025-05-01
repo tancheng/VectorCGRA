7405b0ecc1fcdc9d28d51f9857f9beb0f8c24e83
HobbitQia <861394755@qq.com>
2025-05-01 02:56:57 +0000

    [feat] Support for multi-cycle division

---
+++ fu/single/InclusiveDivRTL.py
@@ -0,0 +1,143 @@
+"""
+==========================================================================
+InclusiveDivRTL.py
+==========================================================================
+Inclusive divisor for CGRA tile.
+
+Author : Cheng Tan
+  Date : November 28, 2019
+"""
+
+from pymtl3 import *
+from ..basic.Fu import Fu
+from ...lib.opt_type import *
+from pymtl3.passes.backends.verilog import *
+
+class InclusiveDivRTL(Fu):
+
+  def construct(s, DataType, PredicateType, CtrlType,
+                num_inports, num_outports, data_mem_size,
+                latency = 8, vector_factor_power = 0):
+
+    super(InclusiveDivRTL, s).construct(DataType, PredicateType, CtrlType,
+                               num_inports, num_outports, data_mem_size,
+                               latency, vector_factor_power)
+
+    num_entries = 2
+    num_cycles = latency
+    FuInType = mk_bits(clog2(num_inports + 1))
+    CountType = mk_bits(clog2(num_entries + 1))
+    OPT_DIV_START = 10
+    OPT_DIV_END   = 11
+
+    s.in0 = Wire(FuInType)
+    s.in1 = Wire(FuInType)
+
+    # TODO: how to parameterize the bit widths
+    s.div = Div(WIDTH=32, CYCLE=num_cycles)
+
+    idx_nbits = clog2(num_inports)
+    s.in0_idx = Wire(idx_nbits)
+    s.in1_idx = Wire(idx_nbits)
+
+    s.in0_idx //= s.in0[0:idx_nbits]
+    s.in1_idx //= s.in1[0:idx_nbits]
+
+    s.recv_all_val = Wire(1)
+
+    @update
+    def comb_logic():
+
+      s.recv_all_val @= 0
+      # For pick input register
+      s.in0 @= 0
+      s.in1 @= 0
+      for i in range(num_inports):
+        s.recv_in[i].rdy @= b1(0)
+      for i in range(num_outports):
+        s.send_out[i].val @= 0
+        s.send_out[i].msg @= DataType()
+
+      s.recv_const.rdy @= 0
+      s.recv_predicate.rdy @= b1(0)
+      s.recv_opt.rdy @= 0
+
+      if s.recv_opt.val:
+        if s.recv_opt.msg.fu_in[0] != 0:
+          s.in0 @= zext(s.recv_opt.msg.fu_in[0] - 1, FuInType)
+        if s.recv_opt.msg.fu_in[1] != 0:
+          s.in1 @= zext(s.recv_opt.msg.fu_in[1] - 1, FuInType)
+
+      if s.recv_opt.val:
+        if (s.recv_opt.msg.operation == OPT_DIV_START):
+          s.div.dividend @= s.recv_in[s.in0_idx].msg.payload
+          s.div.divisor @= s.recv_in[s.in1_idx].msg.payload
+          s.send_out[0].msg.payload @= 0
+          s.send_out[1].msg.payload @= 0
+          s.send_out[0].msg.predicate @= s.recv_in[s.in0_idx].msg.predicate & \
+                                         s.recv_in[s.in1_idx].msg.predicate & \
+                                         (~s.recv_opt.msg.predicate | \
+                                          s.recv_predicate.msg.predicate) & \
+                                         s.reached_vector_factor
+          s.send_out[1].msg.predicate @= s.send_out[0].msg.predicate
+          s.recv_all_val @= s.recv_in[s.in0_idx].val & s.recv_in[s.in1_idx].val & \
+                            ((s.recv_opt.msg.predicate == b1(0)) | s.recv_predicate.val)
+          s.send_out[0].val @= s.recv_all_val
+          s.send_out[1].val @= s.recv_all_val
+          s.recv_in[s.in0_idx].rdy @= s.recv_all_val
+          s.recv_in[s.in1_idx].rdy @= s.recv_all_val
+          s.recv_opt.rdy @= s.recv_all_val
+        elif s.recv_opt.msg.operation == OPT_DIV_END:
+          s.send_out[0].msg.payload @= s.div.quotient
+          s.send_out[1].msg.payload @= s.div.remainder
+          s.send_out[0].msg.predicate @= s.recv_in[s.in0_idx].msg.predicate & \
+                                         s.recv_in[s.in1_idx].msg.predicate & \
+                                         (~s.recv_opt.msg.predicate | \
+                                          s.recv_predicate.msg.predicate) & \
+                                         s.reached_vector_factor
+          s.send_out[1].msg.predicate @= s.send_out[0].msg.predicate
+          s.recv_all_val @= s.recv_in[s.in0_idx].val & s.recv_in[s.in1_idx].val & \
+                            ((s.recv_opt.msg.predicate == b1(0)) | s.recv_predicate.val)
+          s.send_out[0].val @= s.recv_all_val
+          s.send_out[1].val @= s.recv_all_val
+          s.recv_in[s.in0_idx].rdy @= s.recv_all_val & s.send_out[0].rdy
+          s.recv_in[s.in1_idx].rdy @= s.recv_all_val & s.send_out[0].rdy
+          s.recv_opt.rdy @= s.recv_all_val & s.send_out[0].rdy
+
+        else:
+          for j in range(num_outports):
+            s.send_out[j].val @= b1(0)
+          s.recv_opt.rdy @= 0
+          s.recv_in[s.in0_idx].rdy @= 0
+          s.recv_in[s.in1_idx].rdy @= 0
+
+        if s.send_out[0].rdy & (s.recv_opt.msg.predicate == b1(1)):
+          s.recv_predicate.rdy @= s.recv_all_val & s.send_out[0].rdy
+
+class Div( VerilogPlaceholder, Component ):
+
+  # Constructor
+
+  def construct( s, WIDTH = 32, CYCLE = 8 ):
+
+    # Interface
+
+    
+    s.dividend              = InPort ( WIDTH )
+    s.divisor              = InPort ( WIDTH )
+
+    s.quotient            = OutPort ( WIDTH )
+    s.remainder            = OutPort ( WIDTH )
+
+    # Configurations
+
+    from os import path
+    srcdir = path.dirname(__file__) + path.sep
+
+    s.set_metadata( VerilogPlaceholderPass.src_file, srcdir + 'division.v' )
+    s.set_metadata( VerilogPlaceholderPass.top_module, 'pipeline_division' )
+    # s.set_metadata( VerilogPlaceholderPass.v_include, [ srcdir ] )
+    s.set_metadata( VerilogPlaceholderPass.has_clk, True )
+    s.set_metadata( VerilogPlaceholderPass.has_reset, True )
+
+    s.set_metadata( VerilogVerilatorImportPass.vl_Wno_list, ['WIDTH'] )
---
+++ fu/single/division.v
@@ -0,0 +1,105 @@
+module division #(
+    parameter WIDTH = 32,
+    parameter ITER_BEGIN = 0,
+    parameter ITER_END = 32
+) (
+    input  clk,
+    input  reset,
+    input  [WIDTH-1:0] dividend,
+    input  [WIDTH-1:0] divisor,
+    input  [WIDTH:0]   r_i,
+    input  [WIDTH-1:0] q_i,
+    output  [WIDTH:0]   r_o,
+    output  [WIDTH-1:0] q_o
+);
+
+    reg [WIDTH:0] r;      // 33-bit remainder (extra bit for overflow)
+    reg [WIDTH-1:0] q;    // Quotient
+
+    always @(*) begin
+        integer i;
+
+        r = r_i;
+        q = q_i;
+
+        for (i = ITER_BEGIN; i < ITER_END; i = i + 1) begin
+            // Shift remainder left and insert next bit of dividend
+            r = (r << 1) | ((dividend >> (31 - i)) & 1'b1);
+            
+            // Compare and subtract if possible
+            if (r >= divisor) begin
+                r = r - divisor;
+                q[31 - i] = 1'b1;  // Set current quotient bit
+            end else begin
+                q[31 - i] = 1'b0;
+            end
+        end
+    end
+
+    assign r_o = r;
+    assign q_o = q;
+
+endmodule
+
+module pipeline_division #(
+    parameter WIDTH = 32,
+    parameter CYCLE = 9
+) (
+    input  clk,
+    input  reset,
+    input  [WIDTH-1:0] dividend,
+    input  [WIDTH-1:0] divisor,
+    output [WIDTH-1:0] quotient,
+    output [WIDTH-1:0] remainder
+);
+
+    parameter num_div = WIDTH / CYCLE;
+    parameter res_div = WIDTH - CYCLE * num_div;
+
+    reg [WIDTH-1:0] qq[0:CYCLE-1] ;
+    reg [WIDTH:0] rr[0:CYCLE-1] ;
+    wire [WIDTH-1:0] q[0:CYCLE-1] ;
+    wire [WIDTH:0] r[0:CYCLE-1] ;
+
+    genvar i; 
+    generate
+            for (i = 0; i < CYCLE - 1; i++) begin
+                always @(posedge clk) begin
+                    qq[i+1] = q[i];
+                    rr[i+1] = r[i];
+                end
+
+            always @(posedge reset) begin
+                if (reset) begin
+                    for (integer i = 0; i < CYCLE; i++) begin
+                        qq[i] = 0;
+                        rr[i] = 0;
+                    end
+                end
+            end
+        end
+    endgenerate
+
+    generate
+        for (i = 0; i < CYCLE; i++) begin
+            division #(
+                .WIDTH(WIDTH),
+                .ITER_BEGIN(i*num_div),
+                .ITER_END((i+1)*num_div)
+            ) u0 (
+                .clk(clk),
+                .reset(reset),
+                .dividend(dividend),
+                .divisor(divisor),
+                .q_i(qq[i]),
+                .r_i(rr[i]),
+                .q_o(q[i]),
+                .r_o(r[i])
+            );
+        end
+    endgenerate
+
+    assign quotient  = q[CYCLE-1];
+    assign remainder = r[CYCLE-1][WIDTH-1:0];
+
+endmodule
