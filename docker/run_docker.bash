86a9f0f65031232ab05cf356699781b6ca9b6661
yuqisun <yuqi.sun@outlook.com>
2025-05-09 01:35:19 +0800

    Add Dockerfile.

--- docker/run_docker.bash
+++ docker/run_docker.bash
@@ -1,14 +1,8 @@
 #!/bin/bash
 
-export PATH="${HOME}/verilator/bin:$PATH"
-export VERILATOR_ROOT=${HOME}/verilator
-export PYMTL_VERILATOR_INCLUDE_DIR=${HOME}/verilator/share/verilator/include
 echo ${VERILATOR_ROOT}
 echo ${PYMTL_VERILATOR_INCLUDE_DIR}
 verilator --version
 
-cd ${HOME}/cgra/VectorCGRA
-mkdir -p build && cd build
+cd ${HOME}/cgra/VectorCGRA/build
 source ${HOME}/venv/bin/activate
-
-tail -f /dev/null
