0f87a449ef97ef65b9c6c01fad6525384d402148
Jackcuii <forever_1210@yeah.net>
2025-11-04 02:16:30 -0600

    prologue

--- validation/test/dummy.py
+++ validation/test/dummy.py
@@ -22,7 +22,7 @@ class IntraCgraPktTypeDummy:
         return f"IntraCgraPktType({self.first}, {self.second},\n payload = {self.payload})"
 
 class CtrlTypeDummy:
-    def __init__(self, op_code, fu_in_code, tile_in, fu_out, write_reg_from, write_reg_idx, read_reg_from, read_reg_idx):
+    def __init__(self, op_code = None, fu_in_code = None, tile_in = None, fu_out = None, write_reg_from = None, write_reg_idx = None, read_reg_from = None, read_reg_idx = None, routing_xbar_outport = None, fu_xbar_outport = None):
         self.op_code = op_code
         self.fu_in_code = fu_in_code
         self.tile_in = tile_in
@@ -31,19 +31,60 @@ class CtrlTypeDummy:
         self.write_reg_idx = write_reg_idx
         self.read_reg_from = read_reg_from
         self.read_reg_idx = read_reg_idx
+        self.routing_xbar_outport = routing_xbar_outport
+        self.fu_xbar_outport = fu_xbar_outport
+    
+
         
     def __str__(self):
-        fu_in_code_str = f"[{', '.join(str(f) for f in self.fu_in_code)}]"
-        tile_in_str_line0 = f"[{', '.join(str(t) for t in self.tile_in[0:4])},"
-        tile_in_str_line1 = f"{', '.join(str(t) for t in self.tile_in[4:8])}]"
-        fu_out_str_line0 = f"[{', '.join(str(f) for f in self.fu_out[0:4])},"
-        fu_out_str_line1 = f"{', '.join(str(f) for f in self.fu_out[4:8])}]"
-        write_reg_from_str = f"write_reg_from = [{', '.join(str(w) for w in self.write_reg_from)}]"
-        write_reg_idx_str = f"write_reg_idx = [{', '.join(str(w) for w in self.write_reg_idx)}]"
-        read_reg_from_str = f"read_reg_from = [{', '.join(str(r) for r in self.read_reg_from)}]"
-        read_reg_idx_str = f"read_reg_idx = [{', '.join(str(r) for r in self.read_reg_idx)}]"
-        return f"CtrlType({self.op_code},\n {fu_in_code_str},\n {tile_in_str_line0}\n  {tile_in_str_line1},\n {fu_out_str_line0}\n  {fu_out_str_line1},\n {write_reg_from_str},\n {write_reg_idx_str},\n {read_reg_from_str},\n {read_reg_idx_str})"
+        if self.op_code is not None:
+            op_code_str = f"{self.op_code},\n"
+        else:
+            op_code_str = ""
+        if self.fu_in_code is not None:
+            fu_in_code_str = f"[{', '.join(str(f) for f in self.fu_in_code)}],\n"
+        else:
+            fu_in_code_str = ""
+        if self.tile_in is not None:
+            tile_in_str_line0 = f"[{', '.join(str(t) for t in self.tile_in[0:4])}, \n"
+            tile_in_str_line1 = f"{', '.join(str(t) for t in self.tile_in[4:8])}], \n"
+        else:
+            tile_in_str_line0 = ""
+            tile_in_str_line1 = ""
+        if self.fu_out is not None:
+            fu_out_str_line0 = f"[{', '.join(str(f) for f in self.fu_out[0:4])}, \n"
+            fu_out_str_line1 = f"{', '.join(str(f) for f in self.fu_out[4:8])}], \n"
+        else:
+            fu_out_str_line0 = ""
+            fu_out_str_line1 = ""
+        if self.write_reg_from is not None:
+            write_reg_from_str = f"write_reg_from = [{', '.join(str(w) for w in self.write_reg_from)}], \n"
+        else:
+            write_reg_from_str = ""
+        if self.write_reg_idx is not None:
+            write_reg_idx_str = f"write_reg_idx = [{', '.join(str(w) for w in self.write_reg_idx)}], \n"
+        else:
+            write_reg_idx_str = ""
+        if self.read_reg_from is not None:
+            read_reg_from_str = f"read_reg_from = [{', '.join(str(r) for r in self.read_reg_from)}], \n"
+        else:
+            read_reg_from_str = ""
+        if self.read_reg_idx is not None:
+            read_reg_idx_str = f"read_reg_idx = [{', '.join(str(r) for r in self.read_reg_idx)}], \n"
+        else:
+            read_reg_idx_str = ""
+        
+        if self.routing_xbar_outport is not None:
+            routing_xbar_outport_str = f"routing_xbar_outport = [{', '.join(str(r) for r in self.routing_xbar_outport)}], \n"
+        else:
+            routing_xbar_outport_str = ""
+        if self.fu_xbar_outport is not None:
+            fu_xbar_outport_str = f"fu_xbar_outport = [{', '.join(str(f) for f in self.fu_xbar_outport)}], \n"
+        else:
+            fu_xbar_outport_str = ""
+        return f"CtrlType({op_code_str} {fu_in_code_str} {tile_in_str_line0} {tile_in_str_line1} {fu_out_str_line0} {fu_out_str_line1} {write_reg_from_str} {write_reg_idx_str} {read_reg_from_str} {read_reg_idx_str} {routing_xbar_outport_str} {fu_xbar_outport_str})"
 
+            
 class TileInTypeDummy:
     def __init__(self, params):
         self.params = params
@@ -65,7 +106,22 @@ class FuInTypeDummy:
     def __str__(self):
         return f"FuInType({self.params})"
 
-
+class CMD_CONFIG_PROLOGUE_FU_Dummy:
+    def __str__(self):
+        return "CMD_CONFIG_PROLOGUE_FU"
+    
+class CMD_CONFIG_PROLOGUE_ROUTING_CROSSBAR_Dummy:
+    def __str__(self):
+        return "CMD_CONFIG_PROLOGUE_ROUTING_CROSSBAR"
+    
+class CMD_CONFIG_PROLOGUE_FU_CROSSBAR_Dummy:
+    def __str__(self):
+        return "CMD_CONFIG_PROLOGUE_FU_CROSSBAR"
+    
+class CMD_LAUNCH_Dummy:
+    def __str__(self):
+        return "CMD_LAUNCH"
+    
 class CMD_CONFIG_Dummy:
     def __str__(self):
         return "CMD_CONFIG"
@@ -81,7 +137,8 @@ class CMD_CONFIG_COUNT_PER_ITER_Dummy:
 class CMD_CONFIG_TOTAL_CTRL_COUNT_Dummy:
     def __str__(self):
         return "CMD_CONFIG_TOTAL_CTRL_COUNT"
-    
+
+
 class DataTypeDummy:
     def __init__(self, param1, param2):
         self.param1 = param1
