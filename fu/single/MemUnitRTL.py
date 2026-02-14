956d2059ee28831bc53d3af89a1649f185320e5c
yyan7223 <22b305016@stu.hit.edu.cn>
2026-02-14 13:51:47 +0000

    Fixes mem bug again?

--- fu/single/MemUnitRTL.py
+++ fu/single/MemUnitRTL.py
@@ -198,7 +198,7 @@ class MemUnitRTL(Component):
                             s.recv_in[s.in1_idx].val
           s.recv_in[s.in0_idx].rdy @= s.recv_all_val & s.to_mem_waddr.rdy & s.to_mem_wdata.rdy
           s.recv_in[s.in1_idx].rdy @= s.recv_all_val & s.to_mem_waddr.rdy & s.to_mem_wdata.rdy
-          s.to_mem_waddr.msg @= AddrType(s.recv_in[0].msg.payload[0:AddrType.nbits])
+          s.to_mem_waddr.msg @= AddrType(s.recv_in[s.in0_idx].msg.payload[0:AddrType.nbits])
           s.to_mem_waddr.val @= s.recv_all_val
           s.to_mem_wdata.msg @= s.recv_in[s.in1_idx].msg
           s.to_mem_wdata.msg.predicate @= s.recv_in[s.in0_idx].msg.predicate & \
