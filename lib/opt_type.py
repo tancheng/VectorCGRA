c60fdeba37f1e9a93c15aefbda082783819dec49
ShangkunLI <shangkun.li@connect.ust.hk>
2026-03-31 00:30:14 +0800

    add gep op & cmd

--- lib/opt_type.py
+++ lib/opt_type.py
@@ -111,6 +111,11 @@ OPT_LOOP_COUNT                   = OpCodeType( 85 )
 OPT_LOOP_DELIVERY                = OpCodeType( 86 )
 OPT_EXTRACT_PREDICATE            = OpCodeType( 87 )
 
+OPT_GEP                          = OpCodeType( 88 )
+OPT_GEP_CONST                    = OpCodeType( 89 )
+OPT_GEP_2D                       = OpCodeType( 90 )
+OPT_GEP_2D_CONST                 = OpCodeType( 91 )
+
 OPT_SYMBOL_DICT = {
   OPT_START                      : "(start)",
   OPT_NAH                        : "(NAH)",
@@ -202,5 +207,10 @@ OPT_SYMBOL_DICT = {
 
   OPT_LOOP_COUNT                 : "(loop_cnt)",
   OPT_LOOP_DELIVERY              : "(loop_deli)",
-  OPT_EXTRACT_PREDICATE          : "(extract_pred)"
+  OPT_EXTRACT_PREDICATE          : "(extract_pred)",
+
+  OPT_GEP                        : "(gep)",
+  OPT_GEP_CONST                  : "(gep')",
+  OPT_GEP_2D                     : "(gep2d)",
+  OPT_GEP_2D_CONST               : "(gep2d')",
 }
