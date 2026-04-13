35689fcebdcbb08b6ad559e69127290c3a9259b3
yyan7223 <22b305016@stu.hit.edu.cn>
2026-04-13 07:16:29 +0000

    Updates.

--- mem/register_cluster/RegisterClusterRTL.py
+++ mem/register_cluster/RegisterClusterRTL.py
@@ -90,7 +90,8 @@ class RegisterClusterRTL(Component):
             (s.reg_bank[i].send_data.val & reg_towards_fu)
         s.reg_bank[i].send_data.rdy @= s.send_data_to_fu[i].rdy
 
-        s.recv_data_from_routing_crossbar[i].rdy @= (s.inport_opt.operation == OPT_NAH) | (s.inport_opt.fu_in[i] == 0) | s.send_data_to_fu[i].rdy
+        s.recv_data_from_routing_crossbar[i].rdy @= ((s.inport_opt.write_reg_from[i] == PORT_ROUTING_CROSSBAR) \
+                & (s.inport_opt.operation == OPT_NAH)) | s.send_data_to_fu[i].rdy
         s.recv_data_from_fu_crossbar[i].rdy @= 1
         s.recv_data_from_const[i].rdy @= 1
 
